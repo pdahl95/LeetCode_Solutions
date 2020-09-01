960221fda199b4f3569b54e41ca43813cc88553e
Pernille Dahl <pdahl@csumb.edu>
2020-08-31 20:31:49 -0700

    new solutions:

--- LeetCode Solutions/src/Easy/MinCostClimbingStairs.java
+++ LeetCode Solutions/src/Easy/MinCostClimbingStairs.java
@@ -1,4 +1,26 @@
 package Easy;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class MinCostClimbingStairs {
+
+    public static int minCost(int[] cost){
+        int s1 = 0, s2 = 0;
+        for(int i = cost.length-1; i >= 0; i--){
+            int minCost = cost[i] + Math.min(s1, s2);
+            s2 = s1;
+            s1 = minCost;
+        }
+        return Math.min(s1, s2);
+    }
+
+
+    public static void main(String[] args){
+        int[] cost = new int[]{10,15,20};
+        System.out.println( String.format( "Cheapest cost of climbing %s is: %s", Arrays.toString( cost ) , minCost( cost ) ) );
+
+        int[] cost1 = new int[]{1,100,1,1,1,100,1,1,100,1};
+        System.out.println( String.format( "Cheapest cost of climbing %s is: %s", Arrays.toString( cost1 ), minCost( cost1 ) ) );
+    }
 }
--- LeetCode Solutions/src/Easy/SumOfLeftLeaves.java
+++ LeetCode Solutions/src/Easy/SumOfLeftLeaves.java
@@ -1,4 +1,40 @@
 package Easy;
 
 public class SumOfLeftLeaves {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val){this.val = val;}
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static int sumOfLeftLeaves(TreeNode root){
+        if(root == null) return 0;
+        return subtree( root, false );
+    }
+
+    public static int subtree(TreeNode node, boolean isLeft){
+        if(node.left == null && node.right == null){
+            return isLeft ? node.val : 0;
+        }
+        int sum = 0;
+        if(node.left != null){
+            sum += subtree( node.left, true );
+        }
+        if(node.right != null){
+            sum += subtree( node.right, false );
+        }
+        return sum;
+    }
+
+    public static void main(String[] args){
+//        sumOfLeftLeaves();
+    }
 }
